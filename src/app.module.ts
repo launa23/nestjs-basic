499dfdc4ae1325825f065c2bd3a159891990af11
Laun <luan2003hhbg@gmail.com>
2024-06-11 17:19:18 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://luan2003hhbg:twPYf59N3c21hsMa@cluster0.dwpd44g.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
